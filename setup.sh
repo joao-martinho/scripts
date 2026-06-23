40fab513a1536fb78e3c4984e7e6e774c008cf29
João Martinho <joaom101@yahoo.com>
2026-06-23 11:56:31 -0300

    2026-06-23

--- setup.sh
+++ setup.sh
@@ -41,8 +41,8 @@ remover_pacotes=(
 )
 
 repositorios=(
-	"https://github.com/joaoeme/dotfiles.git|/home/joao/Área de trabalho/dotfiles"
-	"https://github.com/joaoeme/joaoeme.github.io.git|/home/joao/Área de trabalho/joaoeme.github.io"
+	"https://github.com/joaom101/dotfiles.git|/home/joao/Área de trabalho/dotfiles"
+	"https://github.com/joaom101/joaom101.github.io.git|/home/joao/Área de trabalho/joaoeme.github.io"
 )
 
 backup_itens=(
