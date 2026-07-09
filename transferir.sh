ddbcd5b60870d5ca05f8b7f9ca01674fcbc6651e
João Martinho <joao@joaomartinho.net.br>
2026-07-09 17:39:41 -0300

    2026-07-09

--- transferir.sh
+++ transferir.sh
@@ -3,45 +3,34 @@
 LOCAL_DIR="/home/joao/Vídeos"
 BASE_USB_DIR="/media/joao/Videoteca"
 
-if [ "$1" != "push" ] && [ "$1" != "pull" ]; then
-	echo "Uso: $0 [push|pull] [series|filmes|animacoes]"
-	exit 1
+if [ "$1" != "series" ] && [ "$1" != "filmes" ] && [ "$1" != "animacoes" ]; then
+    echo "Uso: $0 [series|filmes|animacoes]"
+    exit 1
 fi
 
-if [ "$2" != "series" ] && [ "$2" != "filmes" ] && [ "$2" != "animacoes" ]; then
-	echo "Erro: segundo argumento deve ser 'series', 'filmes' ou 'animacoes'."
-	exit 1
-fi
-
-if [ "$2" = "series" ]; then
-	USB_DIR="$BASE_USB_DIR/_Séries"
-elif [ "$2" = "filmes" ]; then
-	USB_DIR="$BASE_USB_DIR/_Filmes"
+if [ "$1" = "series" ]; then
+    USB_DIR="$BASE_USB_DIR/_Séries"
+elif [ "$1" = "filmes" ]; then
+    USB_DIR="$BASE_USB_DIR/_Filmes"
 else
-	USB_DIR="$BASE_USB_DIR/_Animações"
+    USB_DIR="$BASE_USB_DIR/_Animações"
 fi
 
 if [ ! -d "$LOCAL_DIR" ]; then
-	echo "Erro: Diretório local não existe."
-	exit 1
+    echo "Erro: Diretório local não existe."
+    exit 1
 fi
 
 if [ ! -d "$USB_DIR" ]; then
-	echo "Erro: Diretório do HD externo não existe."
-	exit 1
+    echo "Erro: Diretório do HD externo não existe."
+    exit 1
 fi
 
 echo "----------------------------------------"
 
-if [ "$1" = "push" ]; then
-	echo "Enviando arquivos do LOCAL para o HD EXTERNO..."
-	SRC="$LOCAL_DIR"
-	DEST="$USB_DIR"
-else
-	echo "Trazendo arquivos do HD EXTERNO para o LOCAL..."
-	SRC="$USB_DIR"
-	DEST="$LOCAL_DIR"
-fi
+echo "Enviando arquivos do LOCAL para o HD EXTERNO..."
+SRC="$LOCAL_DIR"
+DEST="$USB_DIR"
 
 echo "Origem: $SRC"
 echo "Destino: $DEST"
@@ -54,9 +43,9 @@ STATUS=$?
 echo "----------------------------------------"
 
 if [ $STATUS -eq 0 ]; then
-	echo "Transferência concluída com sucesso."
+    echo "Transferência concluída com sucesso."
 else
-	echo "Ocorreu um erro durante a transferência."
+    echo "Ocorreu um erro durante a transferência."
 fi
 
 echo "----------------------------------------"
